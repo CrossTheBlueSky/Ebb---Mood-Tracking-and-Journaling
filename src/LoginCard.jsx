d4baa1c2dd1dce83ee4a178094d7c146137f537d
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-07 21:34:32 -0700

    add and delete users

--- src/LoginCard.jsx
+++ src/LoginCard.jsx
@@ -3,20 +3,57 @@ import { useNavigate } from "react-router-dom";
 
 function LoginCard({ appData }) {
   const navigate = useNavigate();
+  const [users, setUsers] = React.useState(appData)
 
-  const allUsers = appData.map((user) => {
+  const allUsers = users.map((user) => {
     return (
       <option key={user.id} value={user.id}>
         {user.name}
       </option>
     );
   });
+  console.log(users)
+
+  function updateUsers(){
+
+    fetch("http://localhost:3000/users")
+    .then(r=>r.json())
+    .then(data=>setUsers(data))
+
+  }
 
   function submitHandler(e){
     e.preventDefault()
-    console.log(e)
+
+    const newUser = {
+      name: e.target.user.value,
+      entryArr : []
+    }
+
+    document.getElementById('user-form').reset()
+    document.getElementById('add-user').close()
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(newUser)
+    }).then(updateUsers)
+
   }
 
+  function deleteHandler(e){
+      e.preventDefault()
+      document.getElementById('delete-user').close()
+
+      fetch(`http://localhost:3000/users/${e.target["delete-choice"].value}`, {
+        method: "DELETE"
+      }).then(updateUsers)
+
+  }
+
+  
   return (
     <div className="flex items-center justify-center h-5/6">
       <div className="card-compact glass w-48 text-neutral-content">
@@ -26,7 +63,7 @@ function LoginCard({ appData }) {
               defaultValue=""
               onChange={(e) => {
                 navigate(`calendar/`, {
-                  state: { userData : appData[e.target.value] },
+                  state: { userData : users[users.findIndex((user)=> user.id === e.target.value)] },
                 });
               }}
             >
@@ -36,17 +73,35 @@ function LoginCard({ appData }) {
               {allUsers}
             </select>
             <button className="btn" onClick={()=>document.getElementById('add-user').showModal()}>Add User</button>
-            <dialog id="add-note" className="modal">
+            <dialog id="add-user" className="modal">
               <div className="modal-box grid grid-cols-1 w-fit">
                 <div className="modal-action place-self-center">
-                <form id="note-form" method="dialog" onSubmit={submitHandler}>
+                <form id="user-form" method="dialog" onSubmit={submitHandler}>
                     <div className="mb-25">
                     <label htmlFor="user">New User</label>
                     <br />
                     <input  type="text" name="user"></input>
                     </div>
                     <button className="btn btn-primary" type="submit">Add User</button>
-                    <div className="btn btn-primary" value="cancel" formMethod="dialog" onClick={()=>document.getElementById('add-note').close()}>Cancel</div>
+                    <div className="btn btn-primary" value="cancel" formMethod="dialog" onClick={()=>document.getElementById('add-user').close()}>Cancel</div>
+                </form> 
+                </div>
+              </div>
+            </dialog>
+            <button className="btn" onClick={()=>document.getElementById('delete-user').showModal()}>Delete User</button>
+            <dialog id="delete-user" className="modal">
+              <div className="modal-box grid grid-cols-1 w-fit">
+                <div className="modal-action place-self-center">
+                <form id="delete-form" method="dialog" onSubmit={deleteHandler}>
+                    <div className="mb-25">
+                    <label htmlFor="delete">Select User To Delete</label>
+                    <br />
+                    <select name="delete-choice">
+                      {allUsers}
+                    </select>
+                    </div>
+                    <button className="btn btn-primary" type="submit">Delete User</button>
+                    <div className="btn btn-primary" value="cancel" formMethod="dialog" onClick={()=>document.getElementById('delete-user').close()}>Cancel</div>
                 </form> 
                 </div>
               </div>
